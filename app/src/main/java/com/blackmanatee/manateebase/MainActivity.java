d0547fec908ee339566dab41cc006eb6203024be
DeCorben <decorben@gmail.com>
2018-01-17 16:32:53 -0500

    conflictstill?

--- app/src/main/java/com/blackmanatee/manateebase/MainActivity.java
+++ app/src/main/java/com/blackmanatee/manateebase/MainActivity.java
@@ -17,21 +17,5 @@ import static com.blackmanatee.manateebase.ShellStream.echo;
 public class MainActivity extends ShellActivity {	
 	//@Override
 	public void engage(View v){
-		try{
-			runSuite(new ManaTBInstanceTest());
-		}
-		catch(Exception ex){
-			ex.printStackTrace(System.out);
-		}
-	}
-	
-	private void runSuite(ShellCase suite) throws Exception{
-		suite.shellFirst();
-		for(int z=0;z<suite.getTestCount();z++){
-			suite.shellBefore();
-			suite.runTest(z);
-			suite.shellAfter();
-		}
-		suite.shellLast();
 	}
 }
